c183f672573e009a5dbf9c1977389428ed59db5f
Matías Vega <elmativega3@gmail.com>
2022-12-08 01:22:32 -0300

    first commit on TypeScript

---
+++ Bases/app.ts
@@ -0,0 +1,8 @@
+const msg:string="hola mundo";//asignacion del tipo de dato.
+//const msg="hola mundo";
+console.log(msg);
+
+
+// let msg1="Hola Mundo";
+// msg1="Matías";
+//msg1=40// no se puede asignar porque es un numero
